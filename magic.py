73620b91a5755dcee3ce9fc5fb17e890d5a68616
praylin <rampoche@macbooks-MacBook-Pro.local>
2016-08-04 00:29:21 -0700

    Random numbers for list elements

--- magic.py
+++ magic.py
@@ -1,3 +1,4 @@
+import random
 
 #Function checks whether the given matrix is a magic square
 def magic_check(matrix):
@@ -20,12 +21,16 @@ def magic_check(matrix):
 matrix = []
 colum_sum = []
 row_sum = []
-print("Enter the elements of magic square:")
 for i in range(0,3):
-    matrix.append([])
-    for j in range(0,3):
-        element = input()
-        matrix[i].append(element)
+    matrix.append(random.sample(range(1, 9), 3))
+
+# s1 = set(matrix[0])
+# s2 = set(matrix[1])
+# s3 = set(matrix[2])
+# print s1.intersection(s2)
+# print s2.intersection(s3)
+# print s3.intersection(s1)
 print("Matrix is:")
 print matrix
+
 magic_check(matrix)
